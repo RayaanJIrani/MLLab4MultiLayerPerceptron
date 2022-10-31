f94877460289d1c02e18d5aa370b6b0f1b2ef1ce
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2022-10-30 23:23:37 -0500

    Working model now

--- main.py
+++ main.py
@@ -267,7 +267,45 @@ def our_model():
 
 
 def his_model():
-    
+    import os
+    import struct
+    # from sklearn.preprocessing import RobustScaler
+
+    def load_mnist(path, kind='fashion_train'):
+        """Load MNIST data from `path`"""
+        labels_path = os.path.join(path, '%s-labels-idx1-ubyte' % kind)
+        images_path = os.path.join(path, '%s-images-idx3-ubyte' % kind)
+
+        with open(labels_path, 'rb') as lbpath:
+            magic, n = struct.unpack('>II', lbpath.read(8))
+            labels = np.fromfile(lbpath, dtype=np.uint8)
+
+        with open(images_path, 'rb') as imgpath:
+            magic, num, rows, cols = struct.unpack(">IIII", imgpath.read(16))
+            images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+
+        return images, labels
+
+    X_train, y_train = load_mnist('/Users/saakethkoka/Documents/MachineLearning/MachineLearningNotebooks/data/', kind='fashion_train')
+    print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
+
+    X_test, y_test = load_mnist('/Users/saakethkoka/Documents/MachineLearning/MachineLearningNotebooks/data/', kind='fashion_t10k')
+    print('Rows: %d, columns: %d' % (X_test.shape[0], X_test.shape[1]))
+
+    X_train = X_train / 255.0 - 0.5
+    X_test = X_test / 255.0 - 0.5
+
+    vals = {'n_hidden': 30,
+            'C': 0.1, 'epochs': 20, 'eta': 0.001,
+            'alpha': 0.001, 'decrease_const': 1e-5, 'minibatches': 50,
+            'shuffle': True, 'random_state': 1}
+
+    model = TLPMiniBatchCrossEntropy(**vals)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    print('Accuracy: %.2f%%' % (accuracy_score(y_test, y_pred)*100))
 
 
 # our_model()
